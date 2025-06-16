452194c7d8ed29efca770ccb1066523da5a57218
chirag0303 <cheekugoyal0303@gmail.com>
2025-06-16 13:31:27 +0530

    Learning MERN

---
+++ day1/app.js
@@ -0,0 +1,23 @@
+console.log("hello from JS!");
+
+// let age =19;
+// const year = 2010;
+// var block = 22;
+
+// console.log(age, year, block);
+// -----------------------------
+
+// console.log(age);
+// var age = 25;
+// console.log(age);
+// var age = 10;
+// console.log(age);
+// -----------------------------
+
+// console.log(age);
+// let age = 22;
+// console.log(age);
+// -----------------------------
+
+console.log('6'/3);  // 2
+
