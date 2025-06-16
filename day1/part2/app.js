5ca2a0079e075c5726bc78336866ee980613577f
chirag0303 <cheekugoyal0303@gmail.com>
2025-06-16 14:07:37 +0530

    function

---
+++ day1/part2/app.js
@@ -0,0 +1,12 @@
+// let juice = 50;
+// let gstOnJuice = 0.1;
+// let fries = 70;
+// let gstOnFood = 0.05;
+
+// console.log(juice + fries + juice*gstOnJuice+ fries*gstOnFood)
+
+function calculateJuiceBill(){
+    let juice = 50;
+    let gstOnJuice = 0.1;
+    console.log(juice + juice * gstOnJuice);
+}
