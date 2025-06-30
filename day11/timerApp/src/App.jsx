7df276662cdc2a374a2dcb83b0dd8b47b72d4540
chirag0303 <cheekugoyal0303@gmail.com>
2025-06-30 13:25:30 +0530

    timerApp + useRev()

--- day11/timerApp/src/App.jsx
+++ day11/timerApp/src/App.jsx
@@ -1,21 +1,41 @@
+import { useRef } from "react";
 import { useEffect, useState } from "react";
 
 const App = () => {
   const [timeInSec, setTime] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
+  const intervalref = useRef(null);
 
   //this code will run again and again as state changes after each render
 
   useEffect(() => {
-    setInterval(()=>{
+
+    intervalref.current =  setInterval(()=>{
       setTime((prev)=> {
         return prev+1;
       });
     },1000);
   },[]);
 
+  const handlePause = () => {
+    const intervalId = intervalref.current;
+    clearInterval(intervalId); 
+    setIsTimerRunning(false);
+  };
+
+  const handlePlay = () => {
+    intervalref.current =  setInterval(()=>{
+      setTime((prev)=> {
+        return prev+1;
+      });
+    },1000);
+    setIsTimerRunning(true);
+  };
+
   return (
     <div>
       <h1>00:00:{timeInSec}</h1>
+      { isTimerRunning ? <button onClick={handlePause}>Pause</button> :  <button onClick={handlePlay}>Play</button>}
     </div>
   );
 };
