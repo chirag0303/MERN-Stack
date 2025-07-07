104fedcaf30b81c52c253eb29ec08c68528fe65f
chirag0303 <cheekugoyal0303@gmail.com>
2025-07-07 14:10:35 +0530

    Create  using API

--- day16/app.js
+++ day16/app.js
@@ -9,41 +9,49 @@ const app = express();
 app.use(express.json()); // this will convey to express-js to read the body of the request in the json format
 
 app.get("/", (req, res) => {
-    res.status(200);
-    res.json({
-        isSuccess: true,
-        message: "Server is running...",
-        data: {},
-    });
+  res.status(200);
+  res.json({
+    isSuccess: true,
+    message: "Server is running...",
+    data: {},
+  });
 });
 
 app.post("/api/v1/products", async (req, res) => {
-    try {
-        const data = req.body;
-        const newProduct = await Product.create(data);
-        res.status(201);
-        res.json({
-            isSuccess: true,
-            message: "Product created!",
-            data: {
-                product: newProduct,
-            },
-        });
-    } catch (err) {
-        console.log("---  error occurred in post products ----");
-        console.log(err.message);
-        console.log("--- -------------- ----");
-        res.status(500);
-        res.json({
-            isSuccess: false,
-            message: "Internal Server Error",
-            data: {
-                errMessage: err.message,
-            },
-        });
+  try {
+    const data = req.body;
+    const newProduct = await Product.create(data);
+    res.status(201);
+    res.json({
+      isSuccess: true,
+      message: "Product created!",
+      data: {
+        product: newProduct,
+      },
+    });
+  } catch (err) {
+    if (err.name === "ValidationError" || err.code == "11000") {
+      res.status(400); // client error
+      console.log("---  error occurred in post products ----");
+      console.log(err.message);
+      res.json({
+        isSuccess: false,
+        message: err.message,
+        data: {},
+      });
+    } else {
+      res.status(500); // server error
+      res.json({
+        isSuccess: false,
+        message: "Internal Server Error",
+        data: {
+          errMessage: err.message,
+        },
+      });
     }
+  }
 });
 
 app.listen(2900, () => {
-    console.log("------ Server started ------");
+  console.log("------ Server started ------");
 });
--- day16/models/product_schema.js
+++ day16/models/product_schema.js
@@ -2,30 +2,37 @@ const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const productSchema = new Schema({
+const productSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     description: String,
     rating: {
-        type: Number,
-        min: 0,
-        max: 5,
+      type: Number,
+      min: 0,
+      max: 5,
     },
     price: {
-        type: Number,
-        min: 0,
-        required: true,
+      type: Number,
+      min: 0,
+      required: true,
     },
     quantity: {
-        type: Number,
-        min: 0,
-        default: 1,
+      type: Number,
+      min: 0,
+      default: 1,
     },
-});
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
-const Product = model("product", productSchema); // name of collection (keep it singular but mongodb will itself make it plural)
+const Product = model("product", productSchema);
 
 module.exports = { Product };
